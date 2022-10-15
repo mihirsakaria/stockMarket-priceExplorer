90e96e5635e8c7681710937cc8704c16887db2ab
MIHIR SAKARIA <mihir.sakaria@unboxrobotics.com>
2022-10-15 21:48:58 +0530

    setting up mongo connection and server

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import connectDB from "./db.js";
+import http from "http";
+
+const PORT = 7000;
+
+const app = express();
+const httpServer = http.createServer(app);
+
+mongoose.connection.on("connected", async () => {
+  try {
+    console.log(`Database connection established`);
+    
+    httpServer.listen(PORT, "0.0.0.0", () => {
+      try {
+        console.log(`Server Running on Port ${PORT}`);
+      } catch (error) {
+        throw error;
+      }
+    });
+  } catch (error) {
+    console.log(`Startup failed : `, error.message);
+    mainLogger.fatal("Startup failed : ", error.message);
+  }
+});
+
+mongoose.connection.on("disconnected", async function () {
+  try {
+    console.log(`MongoDB connection dropped`);
+    console.log("[MONGO DISCONNECT] Purposfully clean exiting the server...");
+  } catch (error) {
+    console.log(
+      "Internal error in disconnect event MongoDB, Message : ",
+      error.message
+    );
+  }
+});
+
+connectDB();
+
+export default app;
