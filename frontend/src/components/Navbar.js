bbbef7f87806751f105ed8dda6f70fc39776c5c7
mihirsakaria <sakaria.1@iitj.ac.in>
2022-10-17 02:37:42 +0530

    navbar bug fix

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ import apiService from '../services/api';
 // Creates a simple navbar
 const Navbar = () => {
   const [stocks, setstocks] = useState([]);
-  
+
   if(stocks.length == 0){
     apiService.get("http://localhost:7000/getListOfAllStocks").then(res =>{
       console.log(res);
@@ -19,7 +19,13 @@ const Navbar = () => {
 
   return (
     <div  className="flex justify-between items-center p-4 bg-red-400 text-white">
-        <h1 className='text-center text-3xl'>Stock View</h1>
+        <h1 
+          className="text-center text-3xl w-10/12 cursor-pointer"
+          onClick={() => {
+            navigate('/');
+          }}>
+            Stock View
+            </h1>
         <Autocomplete
           className='w-2/12 bg-white rounded text-black'
           options={stocks}
