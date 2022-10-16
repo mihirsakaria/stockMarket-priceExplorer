700baed910bbe3685fadb09790454dc88224143c
mihirsakaria <sakaria.1@iitj.ac.in>
2022-10-16 18:56:53 +0530

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import About from './About';
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
